4aff3ffb2be16d8f3b703118cada8b951e24c5f7
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-10-04 12:17:24 -0300

    configure backend, state backend only in the bucket

--- 01-networking/main.tf
+++ 01-networking/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket = "workshop-remote-backend-bucket-180294221572"
+    key    = "networking/terraform.state"
+    region = "us-east-1"
+    use_lockfile = true
+    # dynamodb_table = "workshop-state-lock" - Old Versions
+  }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
