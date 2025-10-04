a4c8ba7552122b852120b645fdcd0128c1501742
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-10-04 11:09:07 -0300

    create state backend and networking

---
+++ 00-state-backend/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.auth.region
+  assume_role { 
+    role_arn = var.auth.assume_role_arn
+  }
+}
---
+++ 01-networking/variables.tf
@@ -0,0 +1,11 @@
+variable "auth" {
+    type = object({
+        region = string
+        assume_role_arn = string
+    })
+
+    default = {
+        assume_role_arn = "arn:aws:iam::180294221572:role/WorkshopNaNuvemRole"
+        region = "us-east-1"
+    }
+}
